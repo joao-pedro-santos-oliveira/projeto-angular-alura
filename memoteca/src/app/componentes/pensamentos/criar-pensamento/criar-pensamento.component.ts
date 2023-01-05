0f788066c6686e6a214289084a9f3bc4e1c354aa
João Oliveira <joaopedro.santosoliveira2003@gmail.com>
2023-01-05 16:47:16 -0300

    commit projeto angular

---
+++ memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css']
+})
+export class CriarPensamentoComponent {
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: 'modelo1'
+  }
+
+  criarPensamento(){
+    alert('pensamento adicionado com sucesso!')
+  }
+
+  cancelarPensamento(){
+    alert('pensamento cancelado com sucesso!')
+  }
+
+}
